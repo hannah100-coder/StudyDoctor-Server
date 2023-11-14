5715dcf01b23661fabd362c0c6794c060696cad6
Dae-Yong-Kim <youbinlove607@gmail.com>
2023-11-14 17:30:33 +0900

    edit chat

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ io.on("connection", (socket) =>{
         socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
     });
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        socket.to(room).emit("new_message", socket.nickname, msg);
         done();
     })
     socket.on("nickname", (nickname) => socket["nickname"] = nickname);
